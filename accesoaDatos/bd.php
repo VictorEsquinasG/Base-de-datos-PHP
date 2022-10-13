c7434aaf96181ac604a11e99806ace2324ec7edd
Fernando Sánchez <fsanjim1208@g.educaand.es>
2022-10-13 12:01:49 +0200

    select acabados

--- accesoaDatos/bd.php
+++ accesoaDatos/bd.php
@@ -1,17 +1,36 @@
 <?php
+abreConexion();
+$resultado = abreConexion()->query("SELECT * FROM producto");
+while ($registro = $resultado->fetch()) {
+    //echo "id ".$registro['ID'].": ".$registro['nombre']."<br />";
+    $producto[$registro['ID']] = [$registro['nombre'],$registro['imagen']];  
+}
+var_dump($producto);
+
+
+function abreConexion(){
+    $dwes = new PDO("mysql:host=localhost;dbname=productos", "root");
+    return $dwes;
+}
 
 function get_by_ID( $id ){
 
-    $dwes = new PDO("mysql:host=localhost;dbname=productos", "root");
-    $resultado = $dwes->query("SELECT * FROM producto where id=$id");
+    abreConexion();
+    $resultado = abreConexion()->query("SELECT * FROM producto where id=$id");
     while ($registro = $resultado->fetch()) {
-        echo "id ".$registro['ID'].": ".$registro['nombre']."<br />";
+        //echo "id ".$registro['ID'].": ".$registro['nombre']."<br />";
+        $producto[$registro['ID']] = [$registro['nombre'],$registro['imagen']];
     }
-
+    return $producto;
 }
 
 function get_All(){
-    
+    abreConexion();
+    $resultado = abreConexion()->query("SELECT * FROM producto");
+    while ($registro = $resultado->fetch()) {
+        $productos[$registro['ID']] = [$registro['nombre'],$registro['imagen']];
+    }
+    return $productos;
 }
 
 function insert_New(){
