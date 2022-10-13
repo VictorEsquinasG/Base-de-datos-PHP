fac09ae52c3a591c3596a851f603c6ec4852bcc5
Víctor <vesqgar3008@g.educaand.es>
2022-10-13 09:56:57 +0200

    Inserción de estilos

--- listados/listado.php
+++ listados/listado.php
@@ -16,10 +16,27 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado</title>
 </head>
 <body>
-    
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Foto</th>
+            <th>Acción</th>
+        </tr>
+        <?php
+            $i = 0;
+            foreach ($lista as $produc) {
+                $foto = "<img src=\"data:image/png;base64, $produc[$i]['foto']\"";
+                print('<tr><td>'.$produc[$i]['id'].'</td><td>'.$produc[$i]['nombre'].'</td><td>'.$foto.'</td>'.
+                '<td> <span>✏</span><span>🗑</span> </td></tr>');
+                $i++;
+            }
+        ?>
+    </table>
 </body>
 </html>
