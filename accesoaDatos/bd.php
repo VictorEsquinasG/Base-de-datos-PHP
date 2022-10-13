11d45dc9066b65761af3304403364a4a715c2e7c
Fernando Sánchez <fsanjim1208@g.educaand.es>
2022-10-13 09:44:17 +0200

    Update bd.php

--- accesoaDatos/bd.php
+++ accesoaDatos/bd.php
@@ -1,11 +1,22 @@
 <?php
 
-/*
-insert new()
-delete by id()
-get by id()
-get all()
-update by id()
-*/
+function get_by_ID(){
 
+}
+
+function get_All(){
+    
+}
+
+function insert_New(){
+    
+}
+
+function delete_by_ID(){
+    
+}
+
+function update_by_ID(){
+    
+}
 
