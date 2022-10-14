29f91ffd762e3c0ca45807e6b921800deb8e1106
Víctor <vesqgar3008@g.educaand.es>
2022-10-14 08:29:39 +0200

    empty-table(filled)

--- listados/listado.php
+++ listados/listado.php
@@ -1,28 +1,26 @@
 <!-- Página del listado -->
 <?php
-    include("../accesoaDatos/bd.php");
-    // if (!isset ($_GET['page']) ) {  
-    //     $pagina = 1;  
-    // } else {  //Si ha sido redireccionado
-    //     $pagina = $_GET['page'];  
-    // }
-    if (isset($_POST['editar']))
-    {
-        //Creamos el objeto 
-        $prod = [];
-        //Actualizamos en la Base de Datos
-        update_by_ID($prod);
-    }else if (isset($_POST['borrar']))
-    {
+include("../accesoaDatos/bd.php");
+// if (!isset ($_GET['page']) ) {  
+//     $pagina = 1;  
+// } else {  //Si ha sido redireccionado
+//     $pagina = $_GET['page'];  
+// }
+if (isset($_POST['editar'])) {
+    //Creamos el objeto 
+    $prod = [];
+    //Actualizamos en la Base de Datos
+    update_by_ID($prod);
+} else if (isset($_POST['borrar'])) {
+}
+$lista = [];
+$lista = get_All();
 
-    }
-    $lista = []; 
-    $lista = get_All();
-    
-   
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -30,6 +28,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado</title>
 </head>
+
 <body>
 
     <form action="../formularios/formulario.php" method="POST">
@@ -37,7 +36,7 @@
     </form>
 
     <h3>Listado</h3>
-      
+
     <table>
         <tr>
             <th>Id</th>
@@ -45,19 +44,26 @@
             <th>Foto</th>
             <th>Acción</th>
         </tr>
-        
+
         <?php
+        if (!empty($lista)) {
             $i = 0;
             foreach ($lista as $produc) {
                 $img = $produc[$i]['foto'];
                 $foto = "<img src=\"data:image/png;base64, $img \"";
-                print('<tr><td>'.$produc[$i]['id'].'</td><td>'.$produc[$i]['nombre'].'</td><td>'.$foto.'</td>'.
-                '<td> <input type="submit" name="editar" value="✏">'.
-                '<input type="submit" name="borrar" value="🗑"> </td></tr>');
+                print('<tr><td>' . $produc[$i]['id'] . '</td><td>' . $produc[$i]['nombre'] . '</td><td>' . $foto . '</td>' .
+                    '<td> <input type="submit" name="editar" value="✏">' .
+                    '<input type="submit" name="borrar" value="🗑"> </td></tr>');
                 $i++;
             }
-            print('<tfoot></tfoot>')
+        }else {
+            print('<tr><td>' . 'No hay alumnos' . '</td><td>' . 'Registrados' . '</td><td>' . 'en la base de datos' . '</td>' .
+                    '<td> <input type="submit" name="editar" value="✏">' .
+                    '<input type="submit" name="borrar" value="🗑"> </td></tr>');
+        }
+        print('<tfoot></tfoot>')
         ?>
     </table>
 </body>
+
 </html>
