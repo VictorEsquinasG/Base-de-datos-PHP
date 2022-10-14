40779fc53dd4955ef5b88944f77dbb19904fc887
Fernando Sánchez <fsanjim1208@g.educaand.es>
2022-10-14 08:11:22 +0200

    insert y select acabados

--- accesoaDatos/bd.php
+++ accesoaDatos/bd.php
@@ -1,12 +1,15 @@
 <?php
-abreConexion();
-$resultado = abreConexion()->query("SELECT * FROM producto");
-while ($registro = $resultado->fetch()) {
-    //echo "id ".$registro['ID'].": ".$registro['nombre']."<br />";
-    $producto[$registro['ID']] = [$registro['nombre'],$registro['imagen']];  
-}
+/*
+    abreConexion();
+    $resultado = abreConexion()->query("SELECT * FROM producto");
+    while ($registro = $resultado->fetch()) {
+        for ($i=0;$registro = $resultado->fetch();$i++){
+             $producto[$i] = [$registro['ID'],$registro['nombre'],$registro['imagen']];
+        }
+       
+    }
 var_dump($producto);
-
+*/
 
 function abreConexion(){
     $dwes = new PDO("mysql:host=localhost;dbname=productos", "root");
@@ -18,8 +21,10 @@ function get_by_ID( $id ){
     abreConexion();
     $resultado = abreConexion()->query("SELECT * FROM producto where id=$id");
     while ($registro = $resultado->fetch()) {
-        //echo "id ".$registro['ID'].": ".$registro['nombre']."<br />";
-        $producto[$registro['ID']] = [$registro['nombre'],$registro['imagen']];
+        for ($i=0;$registro = $resultado->fetch();$i++){
+             $producto[$i] = [$registro['ID'],$registro['nombre'],$registro['imagen']];
+        }
+       
     }
     return $producto;
 }
@@ -27,18 +32,33 @@ function get_by_ID( $id ){
 function get_All(){
     abreConexion();
     $resultado = abreConexion()->query("SELECT * FROM producto");
+    
     while ($registro = $resultado->fetch()) {
         $productos[$registro['ID']] = [$registro['nombre'],$registro['imagen']];
     }
+    
+/*
+    while ($registro = $resultado->fetch()) {
+        for ($i=0;$registro = $resultado->fetch();$i++){
+            $productos[$i] = [$registro['ID'],$registro['nombre'],$registro['imagen']];
+       }
+    }
+    */
     return $productos;
 }
 
-function insert_New(){
-    
+function insert_New( $nombre, $imagen){ 
+
+    abreConexion();
+    $resultado = abreConexion()->query("INSERT INTO producto (nombre, imagem) VALUES ($nombre,$imagen)");
+
 }
 
-function delete_by_ID(){
-    
+function delete_by_ID($id){
+
+    abreConexion();
+    $resultado = abreConexion()->query("DELETE from productos.producto where ID $id");
+
 }
 
 function update_by_ID(){
